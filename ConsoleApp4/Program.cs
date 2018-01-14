79f7968c073985c273ff7f87f9c24b5fceaa15f7
Mitchel <mitchel.veerdonk@outlook.com>
2018-01-14 21:20:51 +0100

    new

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
